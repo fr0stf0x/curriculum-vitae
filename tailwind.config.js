77100038def308a107ca0438c8595bee07de534f
thaotang <tang.xuanthao1996@gmail.com>
2022-03-12 16:03:32 +0700

    upload cv source code

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./main.js",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        display: ['Noah'],
+        body: ['Source Sans Pro']
+      }
+    },
+  },
+  plugins: [],
+}
